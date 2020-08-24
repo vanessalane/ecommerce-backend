677794b5a16327c02c712ba6b659f7db7bcb20e5
Vanessa Lane <vlane@23andme.com>
2020-08-23 15:41:23 -1000

    fix product/category association

--- models/index.js
+++ models/index.js
@@ -5,26 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.Category = Product.belongsTo(Category, {
-  foreignKey: 'product_id'
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
-});
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id'
+  through: ProductTag
 });
 ProductTag.belongsTo(Tag);
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id'
+  through: ProductTag
 });
 ProductTag.belongsTo(Product);
 
